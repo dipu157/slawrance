42dc432e122e10434ce0eb08fa10c644eb9dd033
dipu157 <dipu157@gmail.com>
2024-02-08 16:58:47 +0600

    class create complete

--- app/Models/InstituteInfo/Classes.php
+++ app/Models/InstituteInfo/Classes.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Classes extends Model
 {
     use HasFactory;
+
+    protected $table= 'classes';
+
+    protected $guarded = ['id', 'created_at','updated_at'];
+
+    protected $fillable = [
+        'class_teacher_name',
+        'position',
+        'photo',
+        'class_name',
+        'image',
+        'student_age',
+        'class_time',
+        'capacity',
+        'status',
+        'user_id',
+        
+    ];
 }
