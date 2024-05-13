3b07a5c06dd4d18420c1af050eddd29b70bdbee5
dipu157 <dipu157@gmail.com>
2024-05-13 15:35:10 +0600

    g map added

--- resources/views/Frontend/layout/footer.blade.php
+++ resources/views/Frontend/layout/footer.blade.php
@@ -12,7 +12,7 @@
                             <a class="btn btn-outline-light btn-social" href="{{$institute->social_link4}}"><i class="fab fa-youtube"></i></a>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-6">
+                    <div class="col-lg-3 col-md-6">
                         <h3 class="text-white mb-4">Quick Links</h3>
                         <a class="btn btn-link text-white-50" href="">Home</a>
                         <a class="btn btn-link text-white-50" href="">Academic</a>
@@ -20,28 +20,11 @@
                         <a class="btn btn-link text-white-50" href="">Facility</a>
                         <a class="btn btn-link text-white-50" href="">Contact Us</a>
                     </div>
-                    <div class="col-lg-4 col-md-6">
-                        <h3 class="text-white mb-4">Photo Gallery</h3>
-                        <div class="row g-2 pt-2">
-                            <div class="col-4">
-                                <img class="img-fluid rounded bg-light p-1" src="{{asset("/frontend/img/classes-1.jpg")}}" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img class="img-fluid rounded bg-light p-1" src="{{asset("/frontend/img/classes-2.jpg")}}" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img class="img-fluid rounded bg-light p-1" src="{{asset("/frontend/img/classes-3.jpg")}}" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img class="img-fluid rounded bg-light p-1" src="{{asset("/frontend/img/classes-4.jpg")}}" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img class="img-fluid rounded bg-light p-1" src="{{asset("/frontend/img/classes-5.jpg")}}" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img class="img-fluid rounded bg-light p-1" src="{{asset("/frontend/img/classes-6.jpg")}}" alt="">
-                            </div>
-                        </div>
+                    <div class="col-lg-5 col-md-6">
+                        <h3 class="text-white mb-4">Google Map</h3>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3648.565282292608!2d90.38697447589969!3d23.869565784190044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c568d919b57f%3A0xd191ef1c4263f10f!2sSt.%20Lawrence%20School%20of%20Jewels!5e0!3m2!1sen!2sbd!4v1715592340692!5m2!1sen!2sbd"
+                        width="400" height="250" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
